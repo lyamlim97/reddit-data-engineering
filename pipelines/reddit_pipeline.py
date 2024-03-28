6b9771686d8891374b2428acccd6ec98bc8fcbcc
lyamlim97 <lyamlim97@gmail.com>
2024-03-28 16:51:44 +0800

    Add code to save extracted data to csv.

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,5 +1,6 @@
-from etls.reddit_etl import connect_reddit, extract_posts
-from utils.constants import CLIENT_ID, SECRET
+from etls.reddit_etl import connect_reddit, extract_posts, transform_data, load_data_to_csv
+from utils.constants import CLIENT_ID, SECRET, OUTPUT_PATH
+import pandas as pd
 
 
 def reddit_pipeline(file_name: str, subreddit: str, time_filter="day", limit=None):
@@ -8,6 +9,11 @@ def reddit_pipeline(file_name: str, subreddit: str, time_filter="day", limit=Non
 
     # Extraction
     posts = extract_posts(instance, subreddit, time_filter, limit)
+    post_df = pd.DataFrame(posts)
     # Transformation
-
+    post_df = transform_data(post_df)
     # Loading to CSV
+    file_path = f"{OUTPUT_PATH}/{file_name}.csv"
+    load_data_to_csv(post_df, file_path)
+
+    return file_path
